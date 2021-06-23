342e9cd62dd90a62d31e90097ed52628eaa4837e
Michael Stark <michael.starkb@gmail.com>
2021-06-23 00:14:50 -0400

    weather app js updated and working

--- js/script.js
+++ js/script.js
@@ -1,1 +1,29 @@
-console.log("hello");
+//   const url = "http://api.openweathermap.org/data/2.5/weather?zip=11217,us&appid=ca3c23b7a51bffb90c2bde67910a836d"
+  
+//   $.ajax({
+//     url: url,
+//   }).then(
+//     (data) => {
+//       console.log(data);
+//     },
+//     (error) => {
+//       console.log("bad request", error);
+//     }
+//   );
+
+  async function getWeatherData(e) {
+	e.preventDefault();
+
+	const userInput = $('input[type="text"]').val();
+	const apiKey = "ca3c23b7a51bffb90c2bde67910a836d";
+	const url = `http://api.openweathermap.org/data/2.5/weather?zip=${userInput},us&units=imperial&appid=${apiKey}`;
+
+	const response = await fetch(url);
+	const data = await response.json();
+
+	$("#name").html(data.name);
+	$("#temp").html(data.main.temp);
+	$("#humidity").html(data.main.humidity);
+}
+
+$("form").on("submit", getWeatherData);
